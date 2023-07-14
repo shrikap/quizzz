833c4d5d4cb79b9d230ff0640a7bd0d534961c74
shrikap <shrikapunuganti@gmail.com>
2023-07-14 10:30:36 -0700

    Initial Commit

---
+++ quizzz/quizzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizzzApp.swift
+//  quizzz
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct quizzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
